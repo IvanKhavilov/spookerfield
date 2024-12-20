542825fd6137bf823c9b50641d8c12f514a0429c
IvanKhavilov <73675358+IvanKhavilov@users.noreply.github.com>
2024-12-20 20:31:03 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+$(document).ready(function () {
+  $('.slider__inner').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    adaptiveHeight: false,
+    nextArrow: `<button type="button" class="slick-next button__next"><img class="button__icon" src="../images/slider-btn.webp" width="65" height="65" loading="lazy" alt="button next" /></button>`,
+    prevArrow: `<button type="button" class="slick-prev button__prev"><img class="button__icon" src="../images/slider-btn.webp" width="65" height="65" loading="lazy" alt="button prev" /></button>`,
+  })
+})
+
 document.addEventListener('DOMContentLoaded', () => {
   const burgerMenu = document.getElementById('burgerMenu')
   const navMenu = document.getElementById('navMenu')
@@ -5,46 +16,4 @@ document.addEventListener('DOMContentLoaded', () => {
     burgerMenu.classList.toggle('active')
     navMenu.classList.toggle('active')
   })
-})
-
-document.addEventListener('DOMContentLoaded', () => {
-  const btnNext = document.getElementById('button__next')
-  const btnPrev = document.getElementById('button__prev')
-  const slides = document.querySelectorAll('.slider__item')
-
-  if (btnNext && btnPrev && slides.length > 0) {
-    let index = 0
-
-    const activeSlide = (n) => {
-      slides.forEach((slide) => slide.classList.remove('active'))
-      slides[n].classList.add('active')
-    }
-
-    const prepareCurrentSlide = () => {
-      activeSlide(index)
-    }
-
-    const nextSlide = () => {
-      index = index === slides.length - 1 ? 0 : index + 1
-      prepareCurrentSlide()
-    }
-
-    const prevSlide = () => {
-      index = index === 0 ? slides.length - 1 : index - 1
-      prepareCurrentSlide()
-    }
-
-    btnNext.addEventListener('click', nextSlide)
-    btnPrev.addEventListener('click', prevSlide)
-
-    btnNext.addEventListener('click', () => {
-      nextSlide()
-    })
-
-    btnPrev.addEventListener('click', () => {
-      prevSlide()
-    })
-  } else {
-    console.log('Slider elements not found on this page.')
-  }
 })
