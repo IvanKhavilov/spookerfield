7ae7353beb4925a1f97361775e230632c6e92b8d
IvanKhavilov <73675358+IvanKhavilov@users.noreply.github.com>
2024-12-20 20:58:53 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,10 +3,10 @@ $(document).ready(function () {
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
-    adaptiveHeight: false,
     nextArrow: `<button type="button" class="slick-next button__next"><img class="button__icon" src="images/slider-btn.webp" width="65" height="65" loading="lazy" alt="button next" /></button>`,
     prevArrow: `<button type="button" class="slick-prev button__prev"><img class="button__icon" src="images/slider-btn.webp" width="65" height="65" loading="lazy" alt="button prev" /></button>`,
   })
+  $('.slider__inner').slick('setPosition')
 })
 
 document.addEventListener('DOMContentLoaded', () => {
